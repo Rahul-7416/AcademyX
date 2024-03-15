9ecb856054089fcf6497c36a6e2ed1d73d00b907
rahul-7416 <rj.cu74@gmail.com>
2024-03-15 16:21:43 +0530

    Add login route

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -15,62 +15,131 @@ const generateAccessAndRefreshToken = async (userId) => {
         return { accessToken, refreshToken };
 
     } catch (error) {
+        console.log("Error while generating tokens: ", error);
         throw new ApiError(500, "Something went wrong while generating refreshToken and accessToken");
     }
 }
 
 const registerUser = asyncHandler( async (req, res) => {
 
-    // get user details from the frontend
-    const {name, email, password, isAdmin, referralCode, refferedBy} = req.body;
-
-    // validation -> to check if all the required fields are non-empty
-    if (
-        [name, email, password, isAdmin].some((field) => 
-        field.trim === "")
-    ) {
-        throw new ApiError(400, "All fields are required");
+    try {
+        // get user details from the frontend
+        const {name, email, password, isAdmin, referralCode, refferedBy} = req.body;
+    
+        // validation -> to check if all the required fields are non-empty
+        if (
+            [name, email, password, isAdmin].some((field) => 
+            field.trim === "")
+        ) {
+            throw new ApiError(400, "All fields are required");
+        }
+    
+        // check if the user already exists or not
+        const existedUser = await User.findOne({
+            $or: [ { email } ]
+        })
+    
+        if (existedUser) {
+            throw new ApiError(409, "User with same email already exists!");
+        }
+    
+        // create an entry in the db -> by creating new User document
+        const user = await User.create({
+            name,
+            email,
+            password,
+            isAdmin,
+            referralCode,
+            refferedBy,
+        });
+    
+        // remove password and refresh token from the response 
+        const createdUser = await User.findById(user._id).select(" -password -refreshToken");
+    
+        if (!createdUser) {
+            throw new ApiError(500, "Something went wrong while registering the user");
+        }
+    
+        // return the response -> that data is saved
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                createdUser,
+                "user created successfully"
+            )
+        );
+    } catch (error) {
+        console.log("Error in registerUser: ", error);
+        throw new ApiError(401, "Something went wrong while executing registerUser");
     }
 
-    // check if the user already exists or not
-    const existedUser = await User.findOne({
-        $or: [ { email } ]
-    })
+});
 
-    if (existedUser) {
-        throw new ApiError(409, "User with same email already exists!");
-    }
+const loginUser = asyncHandler( async (req, res) => {
 
-    // create an entry in the db -> by creating new User document
-    const user = await User.create({
-        name,
-        email,
-        password,
-        isAdmin,
-        referralCode,
-        refferedBy,
-    });
-
-    // remove password and refresh token from the response 
-    const createdUser = await User.findById(user._id).select(" -password -refreshToken");
-
-    if (!createdUser) {
-        throw new ApiError(500, "Something went wrong while registering the user");
-    }
-
-    // return the response -> that data is saved
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            createdUser,
-            "user created successfully"
+    try {
+        // get the user login details
+        const { email, password } = req.body;
+    
+        // validation -> to check that data received is not empty
+        if (
+            [email, password].some((fileld) => {
+                fileld.trim === ""
+            })
+        ) {
+            throw new ApiError(400, "All fields are required");
+        }
+    
+        // find the user based on the credentials received
+        const user = await User.findOne({email});
+        
+        if (!user) {
+            throw new ApiError(404, "User not found");
+        }
+    
+        // password check
+        const isPasswordValid = user.isPasswordCorrect(password);
+    
+        if (!isPasswordValid) {
+            throw new ApiError(401, "Invalid user password");
+        }
+    
+        // generate tokens
+        const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
+        console.log("AT: ", accessToken);
+    
+        // removing confedential details from the response
+        const loggedInUser = await User.findById(user._id).select(" -password -refreshToken");
+    
+        // send cookies
+        const options = {
+            httpOnly: true,
+            secure: true,
+        }
+    
+        // return response 
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                loggedInUser,
+                "User logged in successfully"
+            )
         )
-    );
+    } catch (error) {
+        console.log("Error in loginUser: ", error);
+        throw new ApiError(401, "Something went wrong while executing loginUser");
+    }
+
 
 });
 
 export {
     registerUser,
+    loginUser,
 }
