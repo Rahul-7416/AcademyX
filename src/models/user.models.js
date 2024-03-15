3d8e98d3bb7b8f4b04fb800111a25b59d955468e
rahul-7416 <rj.cu74@gmail.com>
2024-03-15 11:19:30 +0530

    implemented bcrypt

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
     {
@@ -35,5 +36,19 @@ const userSchema = new mongoose.Schema(
         timestamps: true
     },
 );
+
+// to hash the password before saving it into the db
+userSchema.pre("save", async function (next) {
+    if(!this.modifiedPaths("password")) return next();
+
+    // hashing the password
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+// custom method -> to compare passwordProvided and prasswordSaved are same or not
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 export const User = mongoose.model("User", userSchema, "Users");
