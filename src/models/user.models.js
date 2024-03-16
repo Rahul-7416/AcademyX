3a8108266c2500d98ba354dfcbd8c7381c32efd6
rahul-7416 <rj.cu74@gmail.com>
2024-03-16 21:54:58 +0530

    fixed some minor bugs

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -26,7 +26,8 @@ const userSchema = new mongoose.Schema(
         },
         referralCode: {
             type: String,
-            unique: true,
+            unique: true, // Unique index for referralCode
+            sparse: true, // Allow multiple documents to have null values for referralCode
         },
         refferedBy: {
             type: mongoose.Schema.Types.ObjectId,
@@ -43,12 +44,12 @@ const userSchema = new mongoose.Schema(
 
 // to hash the password before saving it into the db
 userSchema.pre("save", async function (next) {
-    if(!this.modifiedPaths("password")) return next();
+    if(!this.isModified("password")) return next();
 
     // hashing the password
-    this.password = bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
-})
+});
 
 // custom method -> to compare passwordProvided and prasswordSaved are same or not
 userSchema.methods.isPasswordCorrect = async function (password) {
@@ -64,6 +65,7 @@ userSchema.methods.generateAccessToken = function() {
             _id: this._id,
             email: this.email,
             name: this.name,
+            isAdmin: this.isAdmin,
         },
 
         // secret key 
@@ -85,6 +87,7 @@ userSchema.methods.generateRefreshToken = function() {
             _id: this._id,
             email: this.email, 
             name: this.name,
+            isAdmin: this.isAdmin,
         },
 
         // secret key
